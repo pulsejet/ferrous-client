4f84fb0969e07c68b5f471375adb8cd1b0cb90c4
Varun Patil <radialapps@gmail.com>
2018-03-11 04:13:10 +0530

    Fix home find function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { DataService } from '../data.service';
 
 /* Homepage */
 @Component({
@@ -10,8 +11,20 @@ import { Title } from '@angular/platform-browser';
 export class HomeComponent {
     constructor(private activatedRoute: ActivatedRoute,
         private titleService: Title,
-        public router: Router, @Inject('BASE_URL') baseUrl: string) {
+        public router: Router,
+        public dataService: DataService,
+        @Inject('BASE_URL') baseUrl: string,
+    ) {
         this.titleService.setTitle('Home');
     }
     enteredCL: string;
+
+    FindContingent(clno: string) {
+        this.dataService.NavigateContingentDetails(
+            this.dataService.FillURITemplate(
+                this.dataService.GetLink(this.dataService.GetAPISpec(), 'find_contingent'),
+                { id: clno }
+            )
+        );
+    }
 }
