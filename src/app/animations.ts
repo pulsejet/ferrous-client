d445c9f0e434b3b381d648e27467d9493f0f7609
Varun Patil <radialapps@gmail.com>
2018-03-11 13:31:28 +0530

    Add reflow animation when adding elements This is terrible in terms of performance, but can't find an alternative yet.

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -4,11 +4,13 @@ import {trigger, state, style, transition,
 export const SlideInOutAnimation = [
     trigger('slideInOut', [
         transition(':enter', [
-          style({transform: 'translateX(-20px)', opacity: '0'}),
+          style({transform: 'translateX(-20px)', opacity: '0', 'max-height': '0'}),
+          animate('100ms', style({'max-height': 'none'})),
           animate('200ms ease-in', style({transform: 'translateX(0%)', opacity: '1'})),
         ]),
         transition(':leave', [
-          animate('200ms ease-in', style({transform: 'translateX(20px)', opacity: '0'}))
+          animate('200ms ease-in', style({transform: 'translateX(20px)', opacity: '0'})),
+          animate('100ms', style({height: '0'})),
         ])
     ])
 ];
