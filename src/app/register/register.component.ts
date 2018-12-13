099d1549895abbaee2ea8d26f51ee46dd10fa3d8
Varun Patil <radialapps@gmail.com>
2018-12-13 11:21:34 +0530

    Change M to Male and F to Female in register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -136,7 +136,7 @@ export class RegisterComponent implements OnInit {
 
   /** Get placeholder for name */
   getPlaceholder(person: FormPerson) {
-    return `Person #${person.sno + 1} (${person.sex})`;
+    return `Person #${person.sno + 1} (${person.sex === 'M' ? 'Male' : 'Female'})`;
   }
 
   submit() {
